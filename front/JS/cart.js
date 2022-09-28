02e44b9bff34542c41480e03722b56839c65a0dc
Damien Ouvrard <ouvrard.damien33@gmail.com>
2022-09-28 12:17:19 +0200

    Page_Panier_afficher_produits

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -1,23 +1,58 @@
 // Page Panier 
 
 
-// Déclaration des Variables
 
+// Déclaration des Variables formulaires
+let submitBtn = document.querySelector("#order");
+let firstName = document.querySelector("#firstName");
+let lastName = document.querySelector("#lastName");
+let address = document.querySelector("#address");
+let city = document.querySelector("#city");
+let email = document.querySelector("#email");
 
+// Déclaration des Variables Page Panier 
+let messagePanierVide = document.querySelector("#cartAndFormContainer > h1");
+let messagePanier = document.querySelector("#cart__items");
+
+// Déclaration des Variables 
 
 // Etape 8 : Afficher un tableau récapitulatif des achats dans la page Panier
 
-
-// Récupération du localStorage =>  on récupère toutes les informations stockés 
+//Récupération des infos stocké dans le local storage
 let panier = JSON.parse(localStorage.getItem("produit"));
 
-
-// On va afficher les éléments
-
+//Affichage des éléments du panier
 function afficherPanier() {
-    //Si panier vide
+    // Si panier vide
     if (panier === null || panier.length == 0) {
-        document.querySelector("#cartAndFormContainer > h1").textContent += " est vide";
+        messagePanierVide.textContent += " est vide";
+    }
+    // Si il y a un ou plusieurs éléments dans le panier
+    else {
+        for (let i = 0; i < panier.length; i++) {
+            messagePanier.innerHTML += `
+            <article class="cart__item" data-id="${panier[i][0].idProduct}">
+                <div class="cart__item__img">
+                  <img src="${panier[i][0].image}" alt="${panier[i][0].imageAlt}">
+                </div>
+                <div class="cart__item__content">
+                  <div class="cart__item__content__description">
+                    <h2>${panier[i][0].name}</h2>
+                    <p>${panier[i][0].colorChoice}</p>
+                    <p>${panier[i][0].price}</p>
+                  </div>
+                  <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                      <p>Qté : </p>
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${panier[i][0].qantity}">
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                      <p class="deleteItem">Supprimer</p>
+                    </div>
+                  </div>
+                </div>
+              </article>`;
+        }
     }
 }
-
+afficherPanier();
