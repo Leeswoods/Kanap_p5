c6e14826731038ab3e313ed71fb3192356961e34
Damien Ouvrard <ouvrard.damien33@gmail.com>
2022-10-01 12:59:29 +0200

    Suppression_de_produits_page_panier

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -56,3 +56,40 @@ function afficherPanier() {
     }
 }
 afficherPanier();
+
+// Etape 9 :  Gérer la modification et la suppression de produits dans la page Panier
+
+
+// GERER LA SUPRESSION DE PRODUITS DANS LA PAGE PANIER  
+
+
+  // SELECTION DE L'ELEMENT A SUPPRIMER DANS LE TABLEAU Panier
+  // Première étape on va crée une fonction 
+  // Ensuite on va crée un tableau "buttons" dans cette fonction
+  // On va chercher l'id et la couleur du produit présent dans la classe .deleteItem et le compare au produit présent dans le panier (LocalStorage)
+  // On filtre le produit trouvé et on le supprime du tableau panier 
+  // On enregistre le panier dans le localStorage et on rafraichit la page
+  // Sa envoie une alerte
+
+  // Délcaration Variable => Séléction de class "Supprimer" => Gestion Bouton "Supprimer"
+
+let deleteBtn = document.querySelectorAll(".deleteItem");
+
+
+function deleteProducts() {
+  for (let button of Array.from(deleteBtn)){
+    button.addEventListener("click", e =>{
+      let productID = e.target.getAttribute("productID");
+      let colorChoice = e.target.getAttribute("colorChoice");
+      const searchDeleteItem = panier.find(element => element.id == productID && element.couleur == colorChoice);
+      panier = panier.filter(item => item != searchDeleteItem);
+      localStorage.setItem("produit", JSON.stringify(panier));
+
+      alert("Ce produit a bien été supprimé du panier");
+
+      location.reload()
+    })
+  }
+}
+deleteProducts();
+
