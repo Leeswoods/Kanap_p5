6ee3d532e117986224cd8e0ff5c90f1ef7d72acd
Damien Ouvrard <ouvrard.damien33@gmail.com>
2022-10-05 18:11:02 +0200

    Modification_page_panier

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -1,26 +1,50 @@
 // Page Panier 
 
-// Déclaration des Variables formulaires
-let submitBtn = document.querySelector("#order");
-let firstName = document.querySelector("#firstName");
-let lastName = document.querySelector("#lastName");
-let address = document.querySelector("#address");
-let city = document.querySelector("#city");
-let email = document.querySelector("#email");
+// // Déclaration des Variables formulaires
+// let submitBtn = document.querySelector("#order");
+// let firstName = document.querySelector("#firstName");
+// let lastName = document.querySelector("#lastName");
+// let address = document.querySelector("#address");
+// let city = document.querySelector("#city");
+// let email = document.querySelector("#email");
 
-// Déclaration des Variables Page Panier 
-let messagePanierVide = document.querySelector("#cartAndFormContainer > h1");
-let messagePanier = document.querySelector("#cart__items");
-
-// Déclarations des variables
-let site = "http://localhost:3000/api/products";
+// // Déclaration des Variables Page Panier 
+// let messagePanierVide = document.querySelector("#cartAndFormContainer > h1");
+// let messagePanier = document.querySelector("#cart__items");
 
 
-// Récupérer les  données depuis l'API.
 
-        //Créaction d'une constante
-const fetchProducts = async () => {
-    await fetch(site) // Récupérer l'URL de l'API
+// On récupère l'item "basket" dans le localStorage
+function getBasket() {
+  // Récupération des infos stocké dans le local storage
+  let basket = localStorage.getItem("basket");
+  // Si le panier est vide on crée un tableau
+  if (basket == null){
+      return [];
+  }
+  // Sinon on renvoie l'analyse de la chaîne de caractères "basket" 
+  else{
+      return JSON.parse(basket);
+  }
+}
+getBasket(); // Affiche la fonction
+
+// Cette fonction permet de déclarer le localStorage et l'API
+function getData() {
+
+  // Variable products qui va stocker la Data de l'API
+  let products;
+
+  // Récupération des infos stocké dans le local storage
+  let localStorage = basket();
+
+  //Créaction d'une boucle
+  for (let i = 0; i < localStorage.length; i++) {
+    let id = localStorage[i].id;
+    let colorChoice = localStorage[i].colorChoice;
+    let quantity = localStorage[i].quantity;
+    // Récupérer les  données depuis l'API.
+    fetch('http://localhost:3000/api/products/${productId}')
     .then((res) => {
         return res.json();
     }) // Créaction d'une Promise (Promesse) transforme cette promesse en .json  : la Promise est un objet qui fournit une fonction then qui sera exécutée quand le résultat aura été obtenu
@@ -28,62 +52,125 @@ const fetchProducts = async () => {
       console.log(data);
       return (products = data);
     }) // Permet de traiter la promesse 
-};
-fetchProducts();
-
-
-
-// Déclaration des Variables 
-
-// Etape 8 : Afficher un tableau récapitulatif des achats dans la page Panier
-
-// Fonction permettant d'afficher tous les élements du panier contenu dans le localStorage
-function getCart(){
-  // On fait appel à notre localStorage
-  let basket = JSON.parse(localStorage.getItem("basket"));
-  for(let i in basket){
-  document.querySelector("#cart__items").innerHTML += showCart(basket[i]);
+    
   }
 }
 
 
-//Récupération des infos stocké dans le local storage
-let basket = JSON.parse(localStorage.getItem("basket"));
-console.log(basket);
-//Affichage des éléments du panier
-function showCart(product) {
-    // Si panier vide
-    if (basket === null || basket.length == 0) {
-      document.querySelector("#cartAndFormContainer > h1").textContent += " est vide";
-    }
-    // Si il y a un ou plusieurs éléments dans le panier
-    else {
-          return `
-          <article class="cart__item" data-id="${product.id}" data-color="${product.colorChoice}">
-          <div class="cart__item__img">
-            <img src="${product.Picture}" alt="${product.PictureTxt}">
-          </div>
-          <div class="cart__item__content">
-            <div class="cart__item__content__description">
-              <h2>${product.name}</h2>
-              <p>${product.colorChoice}</p>
-              <p>${product.price} €</p>
-            </div>
-            <div class="cart__item__content__settings">
-              <div class="cart__item__content__settings__quantity">
-                <p>Qté : </p>
-                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
-              </div>
-              <div class="cart__item__content__settings__delete">
-                <p class="deleteItem">Supprimer</p>
-              </div>
-            </div>
-          </div>
-        </article>`;
-        }
-    }
 
-getCart();
+// Etape 8 : Afficher un tableau récapitulatif des achats dans la page Panier
+
+// DOM => Article 
+let productArticle = document.createElement('article'); 
+  productArticle.classList.add('cart_item');
+  productArticle.setAttribute('data-id', '${id}');
+  productArticle.setAttribute('data-color', '${colorChoice}');
+  document.querySelector("#cart__items").appendChild(productArticle);
+
+// DOM => IMAGE
+let productDivImg = document.createElement("div");
+  productDivImg.classList.add('cart__item__img');
+  productDivImg = document.createElement('img');
+  productDivImg.src = '${products.imageUrl}';
+  productDivImg.setAttribute('alt', '${products.altTxt}');
+
+
+// DOM => Container : Description / SETTINGS 
+let productItemContent = document.createElement("div");
+  productItemContent.classList.add('cart__item__content');
+
+// DOM => DESCRIPTION
+let  productItemContentDescription = document.createElement("div");
+  productItemContentDescription.classList.add('cart__item__content__description');
+
+// DOM => DESCRIPTION -> H2 TITRE / P / P
+let productTitle = document.createElement("h2");
+  productTitle.innerHTML = '${products.name}';
+
+// DOM => DESCRIPTION -> PARAGRAPHE COULEUR / P
+let productColor = document.createElement("p");
+  productColor.innerHTML = "Couleur : " + '${colorChoice}';
+
+// DOM => DESCRIPTION -> PARAGRAPHE PRIX
+let productPrice = document.createElement("p");
+  productPrice.innerHTML = '${products.price}' + " €";
+
+
+// DOM => SETTINGS
+let  productItemContentSettings = document.createElement("div");
+  productItemContentSettings.classList.add('cart__item__content__settings');
+
+// DOM => SETTINGS -> QUANTITE 
+let productItemContentSettingsQuantity = document.createElement("div");
+  productItemContentSettingsQuantity.classList.add('cart__item__content__settings__quantity');
+
+// DOM => SETTINGS -> QUANTITE -> PARAGRAPHE (TITRE) QUANTITE
+let productTitleQuantity = document.createElement("p");
+  productTitleQuantity.innerHTML = "Quantité : ";
+
+// DOM => SETTINGS -> QUANTITE -> INPUT QUANTITE
+let productQuantity = document.createElement("input");
+  productQuantity.value = '${quantity}';
+  productQuantity.classList.add = "itemQuantity";
+  productQuantity.setAttribute("type", "number");
+  productQuantity.setAttribute("min", "1");
+  productQuantity.setAttribute("max", "100");
+  productQuantity.setAttribute("name", "itemQuantity");
+
+// DOM => SETTINGS -> BOUTTON DELETE
+let  productItemContentSettingsDelete = document.createElement("div");
+  productItemContentSettingsDelete.classList.add('cart__item__content__settings__delete');
+
+// DOM => SETTINGS -> BOUTTON DELETE -> PARAGRAPHE SUPPRIMER
+let productDelete = document.createElement("p");
+  productDelete.classList.add = "deleteItem";
+  productDelete.innerHTML = "Supprimer";
+
+// FONCTION POUR DEFINIR LA STRUCTURE DU HTML
+function appendChildProducts() {
+  // Cart__items parent de productArticle
+  document.querySelector("#cart__items").appendChild(productArticle);
+
+  // productArticle est le parent de productDivImg
+  productArticle.appendChild(productDivImg);
+
+  // productArticle est le parent de productItemContent
+  productArticle.appendChild(productItemContent);
+
+  // productItemContent est le parent de productItemContentDescription
+  productItemContent.appendChild(productItemContentDescription);
+
+  // productItemContentDescription est le parent de productTitle
+  productItemContentDescription.appendChild(productTitle);
+
+  // productItemContentDescription est le parent de productColor
+  productItemContentDescription.appendChild(productColor);
+
+  // productItemContentDescription est le parent de productPrice
+  productItemContentDescription.appendChild(productPrice);
+
+
+  // productItemContent est le parent de productItemContentSettings
+  productItemContent.appendChild(productItemContentSettings);
+
+  // productItemContentSettings est le parent de productItemContentSettingsQuantity
+  productItemContentSettings.appendChild(productItemContentSettingsQuantity);
+
+  // productItemContentSettingsQuantity est le parent de productTitleQuantity
+  productItemContentSettingsQuantity.appendChild(productTitleQuantity);
+
+
+  // productItemContentSettingsQuantity est le parent de productQuantity
+  productItemContentSettingsQuantity.appendChild(productQuantity);
+
+  // productItemContentSettings est le parent de productItemContentSettingsDelete
+  productItemContentSettings.appendChild(productItemContentSettingsDelete);
+
+  // productItemContentSettingsDelete est le parent de productDelete
+  productItemContentSettingsDelete.appendChild(productDelete);
+}
+
+
 
 // // Etape 9 :  Gérer la modification et la suppression de produits dans la page Panier
   
